d84b99baab569e03db0e9bcd303423c5831b3468
UncleChernomor <96253164+UncleChernomor@users.noreply.github.com>
2022-09-07 20:09:58 +0300

    move folder to assets

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,7 @@
 const { src, dest, parallel, series, watch } = require('gulp');
 const argv = require('yargs').argv;
+const projectConfig = require('./projectInit.json');
+const path = projectConfig.path;
 
 //Live Server
 const browserSync = require('browser-sync').create();
@@ -7,7 +9,8 @@ function browsersync() {
     browserSync.init({ // Инициализация Browsersync
         server: { baseDir: 'src/' }, // Указываем папку сервера
         notify: false, // Отключаем уведомления
-        online: true // Режим работы: true или false
+        online: true, // Режим работы: true или false
+        open: true,
     })
 }
 
