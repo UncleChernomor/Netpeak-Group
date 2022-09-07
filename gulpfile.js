f788f3e904914b04b237870e73d955cd8ae44022
UncleChernomor <96253164+UncleChernomor@users.noreply.github.com>
2022-09-07 18:02:13 +0300

    change name folder app to src

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,7 @@ const argv = require('yargs').argv;
 const browserSync = require('browser-sync').create();
 function browsersync() {
     browserSync.init({ // Инициализация Browsersync
-        server: { baseDir: 'app/' }, // Указываем папку сервера
+        server: { baseDir: 'src/' }, // Указываем папку сервера
         notify: false, // Отключаем уведомления
         online: true // Режим работы: true или false
     })
